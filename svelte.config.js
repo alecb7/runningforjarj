f3525d9c999fad7ac0df3265ba4a52ac118ced8c
Alec Buchanan <alec.buchanan@tesco.com>
2022-05-14 14:14:23 +0100

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,14 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: preprocess({postcss: true}),
 	kit: {
 		prerender : { default: true},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
 	},
 	paths: {
 		base: '/runningforjarj',
