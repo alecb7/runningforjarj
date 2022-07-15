07e86f690cd1226aa59bfd6877d871c3fc129237
Alec Buchanan <alec-buchanan@hotmail.com>
2022-07-15 19:40:51 +0100

    Updates

---
+++ _app/chunks/sectionStyles-0e97f438.js
@@ -0,0 +1,1 @@
+const a=(r,e)=>{if(r.startTime&&!r.endTime&&e==="start")return"background-color: green;";if(r.startTime&&!r.endTime&&e==="end")return";";if(r.startTime&&r.endTime)return"background-color: grey;"},n={laura:["2","14"],duncan:["3","6","9"],ellie:["2","4","7"],swails:["5","8","10","12"],ian:["11","13"],pk:["15","18","20"],baron:["16","19"],terry:["17"]};export{n as d,a as g};
