6f3b1920a03368b6212e2ba1350180893f02ccdd
Alec Buchanan <alec.buchanan@tesco.com>
2022-07-19 11:48:06 +0100

    build

---
+++ src/routes/congrats.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div class="container">
+	<h1>CONGRATS EVERYONE!!!</h1>
+
+	<img src={`${base}/pudding.gif`} alt="pudding" />
+</div>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	img {
+		height: 75vh;
+	}
+</style>
