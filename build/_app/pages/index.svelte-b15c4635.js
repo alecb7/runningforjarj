153280b3fe13bd9df9c58a292ba50d00cb5ee2ed
Alec Buchanan <alec.buchanan@tesco.com>
2022-07-19 12:05:32 +0100

    build

---
+++ build/_app/pages/index.svelte-b15c4635.js
@@ -0,0 +1,63 @@
+import{S as yt,i as bt,s as vt,e as s,k as g,t as h,w as et,c as l,a as d,m as c,h as u,d as i,x as nt,a0 as _t,b as k,g as st,Y as e,y as at,q as ot,o as rt,B as it}from"../chunks/index-f7a6ba17.js";import{b as $t}from"../chunks/paths-396f020f.js";import{g as kt}from"../chunks/Button-06e923c9.js";import{L as jt,I as Ut}from"../chunks/index-332d62e7.js";function Jt(y){let t;return{c(){t=h("Donate now")},l(n){t=u(n,"Donate now")},m(n,f){st(n,t,f)},d(n){n&&i(t)}}}function St(y){let t;return{c(){t=h("favorite")},l(n){t=u(n,"favorite")},m(n,f){st(n,t,f)},d(n){n&&i(t)}}}function qt(y){let t,n,f;return t=new jt({props:{$$slots:{default:[Jt]},$$scope:{ctx:y}}}),n=new Ut({props:{class:"material-icons",$$slots:{default:[St]},$$scope:{ctx:y}}}),{c(){et(t.$$.fragment),et(n.$$.fragment)},l(o){nt(t.$$.fragment,o),nt(n.$$.fragment,o)},m(o,r){at(t,o,r),at(n,o,r),f=!0},p(o,r){const p={};r&1&&(p.$$scope={dirty:r,ctx:o}),t.$set(p);const _={};r&1&&(_.$$scope={dirty:r,ctx:o}),n.$set(_)},i(o){f||(ot(t.$$.fragment,o),ot(n.$$.fragment,o),f=!0)},o(o){rt(t.$$.fragment,o),rt(n.$$.fragment,o),f=!1},d(o){it(t,o),it(n,o)}}}function Bt(y){let t,n,f,o,r,p,_,x,w,L,j,O,K,U,W,I,J,M,G,S,D,F,q,A,R,B,V,C,H,Y,z,P,N,Q,b,X,$,Z,tt,T;return w=new kt({props:{href:"https://www.justgiving.com/fundraising/runningforjarj",variant:"raised",$$slots:{default:[qt]},$$scope:{ctx:y}}}),{c(){t=s("div"),n=s("img"),o=g(),r=s("div"),p=s("h1"),_=h("#RunningForJarj"),x=g(),et(w.$$.fragment),L=g(),j=s("p"),O=h(`On 16 & 17 July 2022, Team Squadron will be running from Keynsham in Bristol
+		to the memorial garden at Loughborough University, raising money for Street
+		Games UK.`),K=g(),U=s("h2"),W=h("Our Story"),I=g(),J=s("p"),M=h(`On 17 July 2014, our friend and teammate Ben Pocock (Jarj) boarded flight
+		MH17 to begin an exciting 6-month placement in Perth, Australia.
+		Unfortunately, he never made it, as flight MH17 was shot down as it flew
+		over Ukraine, killing all 283 passengers and 15 crew members on board. This
+		included our wonderful, lovable friend Jarj.`),G=g(),S=s("p"),D=h(`We all met Jarj at Loughborough University whilst playing Ultimate Frisbee.
+		Whilst he first entered the sport simply because he missed badminton trials
+		and thought he\u2019d give it a go, he quickly became an incredible player and an
+		integral part of the team. On the pitch, he was a real athlete that always
+		pushed himself and his teammates to be better. He would lead the way at
+		training encouraging people to do extra fitness, and was definitely a huge
+		driving force behind our success at a number of competitions.`),F=g(),q=s("p"),A=h(`Off the pitch, Jarj was a very close and valued friend to all of us. He was
+		one of the first in the mix on any night out, always keen for a beer, and
+		not afraid to make a slight fool out of himself (alongside the rest of us!).
+		He was an incredibly special and lovable person to know, with one of the
+		biggest hearts imaginable.`),R=g(),B=s("p"),V=h(`This year will mark 8 years since the downing of flight MH17, and to mark
+		the occasion, Team Squadron had decided to do something slightly wild and
+		run 237km from Keynsham (Ben\u2019s hometown) to Loughborough University. We\u2019ll
+		be starting the challenge in the early hours of the Saturday morning,
+		running as a relay throughout the night and aiming to finish at Loughborough
+		University on the Sunday afternoon. We\u2019ll be running in pairs which means
+		that individually we\u2019ll be covering anywhere between 26 and 62km \u2013 which is
+		no mean feat!`),C=g(),H=s("p"),Y=h(`Planning for the challenge is well underway, and we\u2019d absolutely love for
+		your support with sponsorship as we look to raise money for a fantastic
+		charity that Ben\u2019s family have chosen in his memory \u2013 Street Games UK.`),z=g(),P=s("p"),N=h(`To anyone who knew Ben who might be reading this \u2013 if you would like to get
+		involved in the challenge in some way, we would love to hear from you.
+		There\u2019s lots of ways that you might be able to help, but we\u2019ll also be
+		inviting people to run the last section of the route with us in Ben\u2019s
+		memory.`),Q=g(),b=s("p"),X=h(`To get in touch with us, or to follow our journey to July, head on over to
+		our Instagram page `),$=s("a"),Z=h("@runningforjarj"),tt=h(`
+		and join in the fun!`),this.h()},l(m){t=l(m,"DIV",{class:!0});var a=d(t);n=l(a,"IMG",{src:!0,alt:!0,class:!0}),o=c(a),r=l(a,"DIV",{class:!0});var v=d(r);p=l(v,"H1",{});var lt=d(p);_=u(lt,"#RunningForJarj"),lt.forEach(i),x=c(v),nt(w.$$.fragment,v),v.forEach(i),L=c(a),j=l(a,"P",{});var ht=d(j);O=u(ht,`On 16 & 17 July 2022, Team Squadron will be running from Keynsham in Bristol
+		to the memorial garden at Loughborough University, raising money for Street
+		Games UK.`),ht.forEach(i),K=c(a),U=l(a,"H2",{});var ut=d(U);W=u(ut,"Our Story"),ut.forEach(i),I=c(a),J=l(a,"P",{});var dt=d(J);M=u(dt,`On 17 July 2014, our friend and teammate Ben Pocock (Jarj) boarded flight
+		MH17 to begin an exciting 6-month placement in Perth, Australia.
+		Unfortunately, he never made it, as flight MH17 was shot down as it flew
+		over Ukraine, killing all 283 passengers and 15 crew members on board. This
+		included our wonderful, lovable friend Jarj.`),dt.forEach(i),G=c(a),S=l(a,"P",{});var ft=d(S);D=u(ft,`We all met Jarj at Loughborough University whilst playing Ultimate Frisbee.
+		Whilst he first entered the sport simply because he missed badminton trials
+		and thought he\u2019d give it a go, he quickly became an incredible player and an
+		integral part of the team. On the pitch, he was a real athlete that always
+		pushed himself and his teammates to be better. He would lead the way at
+		training encouraging people to do extra fitness, and was definitely a huge
+		driving force behind our success at a number of competitions.`),ft.forEach(i),F=c(a),q=l(a,"P",{});var mt=d(q);A=u(mt,`Off the pitch, Jarj was a very close and valued friend to all of us. He was
+		one of the first in the mix on any night out, always keen for a beer, and
+		not afraid to make a slight fool out of himself (alongside the rest of us!).
+		He was an incredibly special and lovable person to know, with one of the
+		biggest hearts imaginable.`),mt.forEach(i),R=c(a),B=l(a,"P",{});var gt=d(B);V=u(gt,`This year will mark 8 years since the downing of flight MH17, and to mark
+		the occasion, Team Squadron had decided to do something slightly wild and
+		run 237km from Keynsham (Ben\u2019s hometown) to Loughborough University. We\u2019ll
+		be starting the challenge in the early hours of the Saturday morning,
+		running as a relay throughout the night and aiming to finish at Loughborough
+		University on the Sunday afternoon. We\u2019ll be running in pairs which means
+		that individually we\u2019ll be covering anywhere between 26 and 62km \u2013 which is
+		no mean feat!`),gt.forEach(i),C=c(a),H=l(a,"P",{});var ct=d(H);Y=u(ct,`Planning for the challenge is well underway, and we\u2019d absolutely love for
+		your support with sponsorship as we look to raise money for a fantastic
+		charity that Ben\u2019s family have chosen in his memory \u2013 Street Games UK.`),ct.forEach(i),z=c(a),P=l(a,"P",{});var pt=d(P);N=u(pt,`To anyone who knew Ben who might be reading this \u2013 if you would like to get
+		involved in the challenge in some way, we would love to hear from you.
+		There\u2019s lots of ways that you might be able to help, but we\u2019ll also be
+		inviting people to run the last section of the route with us in Ben\u2019s
+		memory.`),pt.forEach(i),Q=c(a),b=l(a,"P",{});var E=d(b);X=u(E,`To get in touch with us, or to follow our journey to July, head on over to
+		our Instagram page `),$=l(E,"A",{href:!0});var wt=d($);Z=u(wt,"@runningforjarj"),wt.forEach(i),tt=u(E,`
+		and join in the fun!`),E.forEach(i),a.forEach(i),this.h()},h(){_t(n.src,f=`${$t}/jarj.png`)||k(n,"src",f),k(n,"alt","Jarj"),k(n,"class","svelte-1pnq2qd"),k(r,"class","header svelte-1pnq2qd"),k($,"href","https://www.instagram.com/runningforjarj/"),k(t,"class","page svelte-1pnq2qd")},m(m,a){st(m,t,a),e(t,n),e(t,o),e(t,r),e(r,p),e(p,_),e(r,x),at(w,r,null),e(t,L),e(t,j),e(j,O),e(t,K),e(t,U),e(U,W),e(t,I),e(t,J),e(J,M),e(t,G),e(t,S),e(S,D),e(t,F),e(t,q),e(q,A),e(t,R),e(t,B),e(B,V),e(t,C),e(t,H),e(H,Y),e(t,z),e(t,P),e(P,N),e(t,Q),e(t,b),e(b,X),e(b,$),e($,Z),e(b,tt),T=!0},p(m,[a]){const v={};a&1&&(v.$$scope={dirty:a,ctx:m}),w.$set(v)},i(m){T||(ot(w.$$.fragment,m),T=!0)},o(m){rt(w.$$.fragment,m),T=!1},d(m){m&&i(t),it(w)}}}class xt extends yt{constructor(t){super();bt(this,t,null,Bt,vt,{})}}export{xt as default};
